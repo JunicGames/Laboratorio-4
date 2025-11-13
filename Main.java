1a06cf15fc4d9063d3e9eb325082e3257a77016c
Naomi <mar25226@uvg.edu.gt>
2025-11-12 22:29:25 -0600

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,24 +1,21 @@
+// Universidad del Valle de Guatemala
+// Facultad de Ingeniería
+// Programación Orientada a Objetos
+// Segundo Ciclo, 2025
+// Instructor: Ing. Erick Francisco Marroquín Rodríguez
+// Laboratorio 4: Polimorfismo
+// Julio Fernando Ortiz Alvarado – 251190
+// Naomi Saraí Marroquín López - 25226
+// Clase: Main
+// Descripción: clase principal que inicia el programa
+
 import controller.Controller;
-import model.*;
+import view.ConsoleView;
 
 public class Main {
     public static void main(String[] args) {
         Controller controller = new Controller();
-
-        Categoria educacion = new Categoria(1, "Educación");
-        Etiqueta tag1 = new Etiqueta(1, "Tutorial");
-
-        Admin admin = new Admin(1, "Carlos", "carlos@ega.edu");
-        controller.agregarUsuario(admin);
-
-        controller.crearContenido(admin, "articulo", 1, "Introducción a Java", "Conceptos básicos de programación.", educacion);
-        controller.crearContenido(admin, "video", 2, "Polimorfismo en OOP", "Explicación audiovisual.", educacion);
-
-        controller.listarContenidos();
-        controller.filtrarPorTipo("video");
-
-        admin.publicarContenido(new Articulo(3, "Tendencias en IA", "Explorando el futuro de la inteligencia artificial.", educacion));
-
-        controller.generarReporte();
+        ConsoleView view = new ConsoleView(controller);
+        view.mostrarMenu();
     }
 }
